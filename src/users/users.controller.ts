9039895b2e8ebf6f83dde68d840895caec7b8520
Kamal <kamalacca@gmail.com>
2024-04-04 07:26:16 +0400

    add users rest route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // create REST routes for users.
+  @Get() // GET /users
+  findAll(): [] {
+    return [];
+  }
+
+  @Get(':id') // GET /users/123
+  findOne(@Param('id') id: string): object {
+    return { id };
+  }
+  @Post() // POST /users
+  create(@Body() user: object): object {
+    return user;
+  }
+  @Patch(':id') // PATCH /users/123
+  update(@Param('id') id: string, @Body() user: object): object {
+    return { id, ...user };
+  }
+  @Delete(':id') // DELETE /users/123
+  remove(@Param('id') id: string): object {
+    return { id };
+  }
+}
