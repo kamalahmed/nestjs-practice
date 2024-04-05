31586a5fc2083e8996278fa6c25d5b5d17f8dc1b
Kamal <kamalacca@gmail.com>
2024-04-05 12:04:47 +0400

    add employee resource and rename table

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
